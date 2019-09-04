8e779a22556059ffda97c34e3be632e252f274fb
Eli Winkelman <ewink20@icloud.com>
2019-09-04 14:17:04 -0700

    analysis features

--- views/components/AnalysisView.jsx
+++ views/components/AnalysisView.jsx
@@ -1,16 +1,15 @@
 var React = require('react');
-import Plot from 'react-plotly.js';
 
 class AnalysisView extends  React.Component {
 	render() {
 		return (
 			<div>
 			<script>
-				Plotly.newPlot({this.props.analysis.name}, {JSON.stringify(this.props.analysis.data)}, {JSON.stringify(this.props.analysis.layout)});
+				Plotly.newPlot({this.props.analysis.name}, {[this.props.analysis.data]}, {{}});
 			</script>
 			<div className="card">
 				<div className="card-body" id={this.props.analysis.name}>
-					<Plot data={[this.props.analysis.data]} layout={{}} />
+
 				</div>
 			</div>
 			</div>

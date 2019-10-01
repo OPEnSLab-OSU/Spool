654057e9789dad46dff6b65885e202f6edaff15d
Eli Winkelman <ewink20@icloud.com>
2019-10-01 14:33:18 -0700

    Serve with https

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build
-node_modules/.bin/serve -l 3000 -s build &
+node_modules/.bin/serve -l 3000 --ssl-cert /etc/letsencrypt/live/spool.open-sensing.org/fullchain.pem --ssl-key /etc/letsencrypt/live/spool.open-sensing.org/privkey.pem -s build &
 node api/bin/www
